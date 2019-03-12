3f80504758af00b48263834ee47e0369ee61f7c7
Li Li Nian 李立年 <lilinian@crhd0a.crc.hk>
2019-03-12 18:36:25 +0800

    i

--- kubeadm1.sh
+++ kubeadm1.sh
@@ -7,7 +7,7 @@ DNS_VERSION=1.2.0
 PAUSE_VERSION=3.1
 # 基本组件
 docker pull mirrorgooglecontainers/kube-apiserver-amd64:$K8S_VERSION
-docker pull mirrorgooglecontainers/kube-controller-manager-amd64:$K8S_VERSION
+docker pull mirrorgooglecontainers/kube-controller-manager:$K8S_VERSION
 docker pull mirrorgooglecontainers/kube-scheduler-amd64:$K8S_VERSION
 docker pull mirrorgooglecontainers/kube-proxy:$K8S_VERSION
 docker pull mirrorgooglecontainers/etcd-amd64:$ETCD_VERSION
@@ -19,7 +19,7 @@ docker pull quay.io/coreos/flannel:$FLANNEL_VERSION
 
 # 修改tag
 docker tag mirrorgooglecontainers/kube-apiserveramd64:$K8S_VERSION k8s.gcr.io/kube-apiserver-amd64:$K8S_VERSION
-docker tag mirrorgooglecontainers/kube-controller-manageramd64:$K8S_VERSION k8s.gcr.io/kube-controller-manager-amd64:$K8S_VERSION
+docker tag mirrorgooglecontainers/kube-controller-manager:$K8S_VERSION k8s.gcr.io/kube-controller-manager:$K8S_VERSION
 docker tag mirrorgooglecontainers/kube-scheduleramd64:$K8S_VERSION k8s.gcr.io/kube-scheduler-amd64:$K8S_VERSION
 docker tag mirrorgooglecontainers/kube-proxy:$K8S_VERSION k8s.gcr.io/kube-proxy:$K8S_VERSION
 docker tag mirrorgooglecontainers/etcd-amd64:$ETCD_VERSION k8s.gcr.io/etcd-amd64:$ETCD_VERSION
@@ -28,7 +28,7 @@ docker tag coredns/coredns:$DNS_VERSION k8s.gcr.io/coredns:$DNS_VERSION
 
 #删除冗余的images
 docker rmi mirrorgooglecontainers/kube-apiserver-amd64:$K8S_VERSION
-docker rmi mirrorgooglecontainers/kube-controller-manager-amd64:$K8S_VERSION
+docker rmi mirrorgooglecontainers/kube-controller-manager:$K8S_VERSION
 docker rmi mirrorgooglecontainers/kube-scheduler-amd64:$K8S_VERSION
 docker rmi mirrorgooglecontainers/kube-proxy:$K8S_VERSION
 docker rmi mirrorgooglecontainers/etcd-amd64:$ETCD_VERSION
